c3be1e7c0d7b3c6d0c5f8a85ae85005ff12ede17
mandla4ndv <dylanmndlovu@gmail.com>
2025-10-13 19:16:03 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -48,10 +48,10 @@ export default function App() {
 
   // --- Gallery images (replace with your work)
   const galleryImages = useMemo(() => [
-    { src: './src/1.jpg', alt: 'Purple bucket Hat' },
-    { src: 'src/2.jpg', alt: 'Stylish person wearing a ruffle hat' },
-    { src: 'src/3.jpg', alt: 'Baby blue ruffle hat' },
-    { src: 'src/4.jpg', alt: 'Handmade crochet scrunchies' },
+    { src: './main/src/1.jpg', alt: 'Purple bucket Hat' },
+    { src: './src/2.jpg', alt: 'Stylish person wearing a ruffle hat' },
+    { src: '/src/3.jpg', alt: 'Baby blue ruffle hat' },
+    { src: '/main/src/4.jpg', alt: 'Handmade crochet scrunchies' },
     { src: 'src/5.jpg', alt: 'Person wearing baby blue ruffle hat' },
     { src: 'src/6.jpg', alt: 'Person wearing black and white cat hat' },
     { src: 'src/7.jpg', alt: 'Pearl wearing red and white slouchy hat' },
